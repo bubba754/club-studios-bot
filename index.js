8356b7f8b97a932c2517ba5b6d36244eb1e9146a
bubba754 <alejandromanchego420@gmail.com>
2026-03-01 18:15:03 -0500

    server de discord

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ 
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ] 
+});
+
+client.once('ready', () => {
+  console.log('Bot encendido 💙');
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!hola') {
+    message.reply(`Hola ${message.author.username} 🐺✨`);
+  }
+});
+
+client.login('');
